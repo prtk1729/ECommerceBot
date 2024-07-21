17b845271b20c721062e76d0faba542d24dd0f6f
prtk1729 <prateek.pani4243@gmail.com>
2024-07-21 16:41:06 +0530

    improved the responses by Engineering the prompt for queries unrelated to the products

--- ecombot/retrieval_and_generation.py
+++ ecombot/retrieval_and_generation.py
@@ -11,8 +11,9 @@ def generation(vstore):
     PRODUCT_BOT_TEMPLATE = """
     Your ecommercebot bot is an expert in product recommendations and customer queries.
     It analyzes product titles and reviews to provide accurate and helpful responses.
-    Ensure your answers are relevant to the product context and refrain from straying off-topic.
-    Your responses should be concise and informative.
+    Ensure your answers are relevant to the product context and refrain from off-topic responses.
+    Your responses should be concise and informative. In case, user asks for generic queries 
+    unrelated to the products, use your own knowledge to answer them.
 
     CONTEXT:
     {context}
